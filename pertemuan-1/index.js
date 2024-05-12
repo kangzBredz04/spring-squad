5aa6b705b7e9b9159dc0da7b8462d6d09dd9327a
wahyu <wabredz1234@gmail.com>
2024-05-12 12:38:57 +0700

    refactor: Update index.ts

--- pertemuan-1/index.js
+++ pertemuan-1/index.js
@@ -1,4 +1,19 @@
+// unions ( | )
 var bilangan = 21;
+bilangan = "duapuluhsatu";
 console.log(bilangan);
 var message = "Hello World";
 console.log(message);
+function cetakNama(orang) {
+    console.log(orang.nama);
+}
+var orang1 = {
+    id: 1,
+    nama: "Prabowo",
+};
+var mahasiswa = {
+    id: 1,
+    nama: "Prabowo",
+    fakultas: "Militer",
+};
+console.log(mahasiswa.fakultas);
--- pertemuan-1/index.ts
+++ pertemuan-1/index.ts
@@ -1,5 +1,51 @@
-let bilangan = 21;
+// INFERENTION
+let bilangan: number = 21;
 console.log(bilangan);
 
 let message: string = "Hello World";
 console.log(message);
+
+interface Orang {
+  id: number;
+  nama: string;
+}
+
+function cetakNama(orang: Orang): void {
+  return console.log(orang.nama);
+}
+
+// cetakNama();
+
+interface Mahasiswa extends Orang {
+  fakultas: string;
+}
+interface Dosen extends Orang {
+  matakuliah: string;
+}
+
+let orang1: Orang = {
+  id: 1,
+  nama: "Prabowo",
+};
+
+let mahasiswa: Mahasiswa = {
+  id: 1,
+  nama: "Prabowo",
+  fakultas: "Militer",
+};
+
+console.log(mahasiswa.fakultas);
+
+interface Komunitas<Type> {
+  id: number;
+  nama: string;
+  cetakAnggota: () => Type;
+}
+
+const komunitas1: Komunitas<Mahasiswa> = {
+  id: 0,
+  nama: "",
+  cetakAnggota: function (): Mahasiswa {
+    throw new Error("Function not implemented.");
+  },
+};
